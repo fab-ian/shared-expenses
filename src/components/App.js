4a48acc24e6c50372aa00bb6e1f22a2ad0ebb5bd
Fabian Kedzierski <grafska@gmail.com>
2017-09-22 16:07:06 +0200

    Add Bootstrap 4

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,48 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h2>Welcome to Shared Expenses</h2>
+      <div>
+        <nav className="navbar navbar-expand-md navbar-dark bg-primary fixed-top">
+          <a className="navbar-brand" href="">Shared Expenses</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarsExampleDefault">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item active">
+                <a className="nav-link" href="">Home <span className="sr-only">(current)</span></a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="">Link</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link disabled" href="">Disabled</a>
+              </li>
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+                <div className="dropdown-menu" aria-labelledby="dropdown01">
+                  <a className="dropdown-item" href="">Action</a>
+                  <a className="dropdown-item" href="">Another action</a>
+                  <a className="dropdown-item" href="">Something else here</a>
+                </div>
+              </li>
+            </ul>
+            <form className="form-inline my-2 my-lg-0">
+              <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" />
+              <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+          </div>
+        </nav>
+
+        <div className="container">
+
+          <div className="starter-template">
+            <h1>Bootstrap starter template</h1>
+            <p className="lead">Use this document as a way to quickly start any new project.<br /> All you get is this text and a mostly barebones HTML document.</p>
+          </div>
+        </div>
+
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,5 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
+import './css/index.css'
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
