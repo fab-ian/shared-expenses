6baa5a0ac86e246d317b600277b0c5123c5d0d91
Fabian Kedzierski <grafska@gmail.com>
2017-09-25 11:40:16 +0200

    Add components: Signin and Header, set route to 'signin' component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter } from 'react-router-dom'
 
+import App from './components/App';
+
+import registerServiceWorker from './registerServiceWorker';
 import './css/index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+  , document.getElementById('root')
+);
+
 registerServiceWorker();
