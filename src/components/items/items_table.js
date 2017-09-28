6e32445e28e3de84d4e9cbf750906925429d8718
Fabian Kedzierski <grafska@gmail.com>
2017-09-28 15:55:22 +0200

    Implement component, action and reducer for 'ItemsTable'

---
+++ src/components/items/items_table.js
@@ -0,0 +1,48 @@
+import React, {Component} from 'react'
+import * as items_actions from '../../actions/items'
+import { connect } from 'react-redux'
+
+class ItemTable extends Component {
+  componentWillMount(){
+    this.props.items_table()
+  }
+
+  render(){
+    let tr = []
+
+    if(this.props.items_content) {
+      tr = this.props.items_content.map(item => (
+      <tr key={item.id}>
+        <th scope="row">{item.id}</th>
+        <td>{item.name}</td>
+        <td>{item.description}</td>
+        <td></td>
+      </tr>
+    ))
+    }
+
+    return(
+      <div>
+        <table className="table table-hover">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Name</th>
+              <th>Description</th>
+              <th>Date</th>
+            </tr>
+          </thead>
+          <tbody>
+            {tr}
+          </tbody>
+        </table>        
+      </div>
+    )
+  }
+}
+
+function mapStateToProps(state){
+  return {items_content: state.items.items_table }
+}
+
+export default connect(mapStateToProps, items_actions)(ItemTable)
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux'
 import { reducer as formReducer } from 'redux-form'
 import authReducer from './auth'
+import itemsReducer from './items'
 
 const rootReducer = combineReducers({
   auth: authReducer,
-  form: formReducer
+  form: formReducer,
+  items: itemsReducer
 });
 
 export default rootReducer
