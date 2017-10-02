b5c0921cf305a736c2112485add36571e0164e64
Fabian Kedzierski <grafska@gmail.com>
2017-10-02 19:28:32 +0200

    Implement component 'ItemAdd' using modal window

--- src/components/items/index.js
+++ src/components/items/index.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
 import ItemsTable from './items_table'
+import ItemAdd from './item_add'
 
 class Items extends Component {
   render(){
     return(
       <div>
-        <h2>Items</h2>
+        <h2>
+          Items
+          <ItemAdd buttonLabel="Add Item"/>        
+        </h2>
         <ItemsTable />
       </div>
     )
